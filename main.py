c4be4ca3f4df8833347db3864dffb44836daa6d8
KetoIT <103219032+KetoIT@users.noreply.github.com>
2023-08-02 13:33:42 +0300

    Update main.py

--- main.py
+++ main.py
@@ -112,13 +112,8 @@ def second(message, dogovor):
 
 def first(message):
     dogovor = message.text
-    try:
-        dogovor = int(dogovor)
-        bot.send_message(message.from_user.id, f"Отлично, номер договора: {dogovor}\n\nТеперь введите сумму:")
-        bot.register_next_step_handler_by_chat_id(message.chat.id, second, dogovor)
-    except ValueError:
-        bot.send_message(message.from_user.id, "Неверный формат! Попробуйте еще раз.")
-        bot.register_next_step_handler_by_chat_id(message.chat.id, first)
+    bot.send_message(message.from_user.id, f"Отлично, номер договора: {dogovor}\n\nТеперь введите сумму:")
+    bot.register_next_step_handler_by_chat_id(message.chat.id, second, dogovor)
 
 
 @bot.message_handler(content_types=['text'])

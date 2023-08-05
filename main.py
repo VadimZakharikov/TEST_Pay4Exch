2a55271fd3081701f08a89c6d69c2ed7b5e724e7
moonalom <102826967+Moonalom@users.noreply.github.com>
2023-08-05 14:16:45 +0300

    Update main.py
    update

--- main.py
+++ main.py
@@ -64,16 +64,18 @@ def create_link(number, summ):
                          digestmod=hashlib.sha1).digest()
     signature_base64 = base64.b64encode(signature).decode()
     headers = {
-        'TCB-Header-Login': "T3678102697ID",
+        'TCB-Header-Login': config.LOGIN,
         'TCB-Header-Sign': signature_base64,
         "Content-Type": "application/json; charset=utf-8"
     }
-    responseJSON = requests.get("https://paytest.online.tkbbank.ru/api/v1/card/unregistered/debit",
-                                data=json.dumps(parameters, ensure_ascii=False).encode('utf-8'),
-                                headers=headers)
-    response = responseJSON.json()
-    print(response)
-    return f"Ссылка: {response['FormURL']}"
+    try:
+        responseJSON = requests.get("https://paytest.online.tkbbank.ru/api/v1/card/unregistered/debit",
+                                    data=json.dumps(parameters, ensure_ascii=False).encode('utf-8'),
+                                    headers=headers)
+        response = responseJSON.json()
+        return f"Ссылка: {response['FormURL']}"
+    except TimeoutError:
+        return f"Ошибка: timeout error"
 
 
 @bot.message_handler(commands=["pay"])
@@ -91,51 +93,55 @@ def start(message):
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback_inline(call):
+    bot.clear_step_handler_by_chat_id(chat_id=call.message.chat.id)
     if call.data == 'yes':
         bot.send_message(call.from_user.id,
                          f"Отлично!\n\n<i>{create_link(str(kvatance['id']), float(kvatance['price']) * 100)}</i>",
                          parse_mode="HTML", reply_markup=ReplyKeyboardMarkup(resize_keyboard=True).add("Оплатить"))
+
         return
     elif call.data == 'no':
         buttons = ["Оплатить"]
         bot.send_message(call.from_user.id, "Оплата отменена!",
                          reply_markup=ReplyKeyboardMarkup(resize_keyboard=True).add(*buttons))
         return
-
-
+voc = "абвгдийклмнопрстуфхцчшщъыьэюяabcdefghijklmnopqrstuvwxyz"
 def second(message, dogovor):
     summa = message.text
+    summa = str.replace(summa, ",", ".")
+    for smb in voc:
+        if smb in summa.lower():
+            bot.send_message(message.from_user.id, "Неверная сумма, введите ещё раз.")
+            bot.register_next_step_handler_by_chat_id(message.chat.id, second, dogovor)
+            return
     global kvatance
     try:
         summa = float(summa)
         btn1 = InlineKeyboardButton("Ок", callback_data="yes")
         btn2 = InlineKeyboardButton("Отмена", callback_data="no")
+        summa = round(summa, 2)
         kvatance = {"id": dogovor, "price": summa}
         bot.send_message(message.from_user.id,
-                         f"Квитанция: <i>{dogovor}</i>\nСумма: <i>{summa} руб.</i>\n\n<b>Все верно?</b>",
+                         f"Квитанция: <i>{dogovor}</i>\nСумма: <i>{summa:.2f} руб.</i>\n\n<b>Все верно?</b>",
                          parse_mode="HTML", reply_markup=InlineKeyboardMarkup().add(btn1, btn2)),
     except ValueError:
         bot.send_message(message.from_user.id, "Неверный формат! Попробуйте еще раз.")
         bot.register_next_step_handler_by_chat_id(message.chat.id, second, dogovor)
-
-
 def first(message):
     dogovor = message.text
-    bot.send_message(message.from_user.id, f"Отлично, номер договора: {dogovor}\n\nТеперь введите сумму:")
+    bot.send_message(message.from_user.id, f"Введите сумму платежа:")
     bot.register_next_step_handler_by_chat_id(message.chat.id, second, dogovor)
 
 
 @bot.message_handler(content_types=['text'])
 def message_handler(message):
     userid = message.from_user.id
-    user = message.from_user.username
     if message.text == "Оплатить":
         bot.send_message(userid, "Введите номер договора: ")
         bot.register_next_step_handler_by_chat_id(message.chat.id, first)
     else:
         bot.send_message(userid, "Простите, но я не знаю такую команду :<")
-
-
+#================================================================================================
 #@bot.message_handler(commands=["pay"])
 #def pay(message):
 #    bot.send_message(message.chat.id, 'Укажите номер заявки:')

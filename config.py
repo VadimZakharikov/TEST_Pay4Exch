22ed612ea802b475e8f59babf8264f533ab2d03a
KetoIT <103219032+KetoIT@users.noreply.github.com>
2023-08-05 14:11:29 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,4 @@
 BOT_TOKEN = "6212783247:AAH-rrdrgpA-7x1KjAW4JXr4Z6-IOSpOJ18"
-APP_URL = "https://pay4exch-telegram.herokuapp.com/" + BOT_TOKEN
-DB_URI = "postgres://dsyasedeqdffle:506ea860d88423c177181b2ab6a9740f7267e22c0a4676263fab20d739494e56@ec2-23-20-211-19.compute-1.amazonaws.com:5432/dc3jofarp6a3fj"
 API_KEY="k6WRcPWcVCpuLPDJoJ7hYLDtsqZF6nMnD8UxKcqNCVyfkNJ1AYdbk35KCDyWZreJZc0L4g7mtvPcmxhPQ7eijKcJdj3gOCXkQZpiV66uZ1SZp2yevTf0n5zq8sHUm0GZGDvvh82SaTsr1nujVYV3w57UA8iDznh7u2sUGc5vZw0COhxW6x7wfNCLEL3iZztXMt583JMS2zeaeFfsMvFboU2RzQp5hXEzddZvmy1yUqDQHCF8FLFE3rK1zoJotQLe"
 LOGIN="T3678102697ID"
+PAY_URL="https://paytest.online.tkbbank.ru/api/v1/card/registered/debi"

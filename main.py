e2da84329681e8ade183a2853276e121031b6900
moonalom <102826967+Moonalom@users.noreply.github.com>
2023-08-08 17:01:44 +0300

    Update main.py

--- main.py
+++ main.py
@@ -71,8 +71,7 @@ def create_link(number, summ):
     headers = {
         'TCB-Header-Login': LOGIN,
         'TCB-Header-Sign': signature_base64,
-        "Content-Type": "application/json; charset=utf-8",
-        'URL': "https://172.16.78.90:8885/process_data"
+        "Content-Type": "application/json; charset=utf-8"
     }
     try:
         responseJSON = requests.get("https://paytest.online.tkbbank.ru/api/v1/card/unregistered/debit",

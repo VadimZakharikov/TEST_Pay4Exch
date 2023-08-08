91d893a4588f8322134aea3c8722f403a9828ced
moonalom <102826967+Moonalom@users.noreply.github.com>
2023-08-08 18:45:17 +0300

    Update config.py
    update

--- config.py
+++ config.py
@@ -3,4 +3,5 @@ API_KEY="k6WRcPWcVCpuLPDJoJ7hYLDtsqZF6nMnD8UxKcqNCVyfkNJ1AYdbk35KCDyWZreJZc0L4g7
 LOGIN="T3678102697ID"
 APP_URL = "https://pay4exch-telegram.herokuapp.com/" + BOT_TOKEN
 DB_URI = "postgres://dsyasedeqdffle:506ea860d88423c177181b2ab6a9740f7267e22c0a4676263fab20d739494e56@ec2-23-20-211-19.compute-1.amazonaws.com:5432/dc3jofarp6a3fj"
-PAY_URL="https://paytest.online.tkbbank.ru/api/v1/card/registered/debi"
+PAY_URL="https://paytest.online.tkbbank.ru/api/v1/card/unregistered/debit"
+STAT_URL="https://paytest.online.tkbbank.ru/api/v1/order/state"

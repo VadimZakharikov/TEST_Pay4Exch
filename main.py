dcd31fa75f5e0d164046be9b156be183debfbf67
moonalom <102826967+Moonalom@users.noreply.github.com>
2023-08-08 17:02:40 +0300

    Update main.py

--- main.py
+++ main.py
@@ -346,4 +346,4 @@ def redirect_message():
 if __name__ == "__main__":
     bot.remove_webhook()
     bot.set_webhook(url=APP_URL)
-    server.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), ssl_context=('cert.pem', 'key.pem'))
+    server.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))

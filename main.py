e0fd6887ea68a3b7c2be2bb487637f5bc07c1619
moonalom <102826967+Moonalom@users.noreply.github.com>
2023-08-08 16:56:43 +0300

    Update main.py

--- main.py
+++ main.py
@@ -342,12 +342,12 @@ def process_data():
     print("\n\\Переданные данные:", data)
     return "Успех!"
 
-@server.route(f"/{BOT_TOKEN}", methods=["POST"])
-def redirect_message():
-    json_string = request.get_data().decode("utf-8")
-    update = telebot.types.Update.de_json(json_string)
-    bot.process_new_updates([update])
-    return "!", 200
+#@server.route(f"/{BOT_TOKEN}", methods=["POST"])
+#def redirect_message():
+#    json_string = request.get_data().decode("utf-8")
+#    update = telebot.types.Update.de_json(json_string)
+#    bot.process_new_updates([update])
+#    return "!", 200
 
 
 if __name__ == "__main__":

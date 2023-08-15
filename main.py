338002d2e1adf166d7e336c4d9d2d67a53b564c9
moonalom <102826967+Moonalom@users.noreply.github.com>
2023-08-15 14:07:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -221,6 +221,6 @@ def redirect_message():
 
 if __name__ == "__main__":
     bot.remove_webhook()
-    #bot.set_webhook(url=APP_URL)
+    bot.set_webhook(url=APP_URL)
     #bot.infinity_polling()
     server.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
